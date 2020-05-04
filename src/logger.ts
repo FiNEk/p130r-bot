d422fc1fba2210040edc4bf3005a4558753758b7
Anashkin Denis <fnk007@yandex.ru>
2020-05-04 22:58:29 +0300

    fix timestamp

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,18 @@
 import { createLogger, format, transports } from "winston";
 import path from "path";
+
 const { combine, printf } = format;
 const rootFolder = path.resolve(__dirname, "../");
 const myFormat = printf((info) => `${info.timestamp} [${info.level}]: - ${info.message}`);
+const appendTimestamp = format((info) => {
+  info.timestamp = new Date().toJSON();
+  return info;
+});
+
 export const logger = createLogger({
   level: "info",
   exitOnError: true,
-  format: combine(myFormat),
+  format: combine(appendTimestamp(), myFormat),
   transports: [
     new transports.File({
       filename: "bot-error.log",
