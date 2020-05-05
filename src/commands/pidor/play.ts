fd56a2ec9c474c00e4c9821bcd2ae35bf6c8be45
Bloodeg <bloodeg@gmail.com>
2020-05-05 20:01:38 +0200

    Added basic gay of the day gameplay

---
+++ src/commands/pidor/play.ts
@@ -0,0 +1,33 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { logger } from "../../logger";
+import { Game } from "../../core/game";
+import { User, Message } from "discord.js";
+
+export default class Play extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: "play",
+      aliases: ["pidor", "pidor-play", "pidoroll"],
+      group: "pidor",
+      memberName: "play",
+      description: "Выбирает победителя конкурса 'Пидор дня' на текущем сервере.",
+    });
+  }
+
+  async run(message: CommandoMessage) {
+    try {
+      const game = new Game(message.guild.id);
+      const result = await game.getTodayResult();
+      const winnerUser = await new User(this.client, {id: result.result?.winnerId }).fetch();
+      const winnerMember = await message.guild.member(winnerUser)?.fetch();
+      if (result.isNew) {
+        return message.say(`Победителем сегодняшней олимпиады становится ${winnerMember?.displayName ?? winnerMember?.user.username ?? 'какой-то хуй'}`);
+      } else {
+        return message.say(`Пидор дня уже определен, это ${winnerMember?.displayName ?? winnerMember?.user.username ?? 'какой-то хуй'}`);
+      }
+    } catch (error) {
+      logger.error(error);
+      return message.reply("что-то пошло не так.");
+    }
+  }
+}
---
+++ src/core/game.ts
@@ -0,0 +1,41 @@
+import Database from "./db";
+import _ from "lodash";
+import Pidor from "../entity/Result";
+import { logger } from "../logger";
+
+export class Game {
+  private readonly guildId: string;
+  
+  constructor(guildId: string) {
+    this.guildId = guildId;
+  }
+
+  async getTodayResult(): Promise<{ result: Pidor | undefined, isNew: boolean }> {
+      const date = (new Date().setUTCHours(0, 0, 0, 0) - 10800000) / 1000;
+      return this.getResult(date);
+  }
+
+  async getResult(date: number): Promise<{ result: Pidor | undefined, isNew: boolean }> {
+    try {
+      let result = await Database.getResult(this.guildId, date);
+      if (result === undefined) {
+        const user = await this.roll();
+        if (user !== undefined) {
+          await Database.addResult(this.guildId, date, user);
+          result = await Database.getResult(this.guildId, date);
+        }
+        return { result: result, isNew: true };
+      } else {
+        return { result: result, isNew: false };
+      }
+    } catch (error) {
+      logger.error(error);
+      return { result: undefined, isNew: false };
+    }
+  }
+
+  async roll(): Promise<string | undefined> {
+    const players = await Database.getPlayers(this.guildId);
+    return _.sample(players)?.id;
+  }
+}
