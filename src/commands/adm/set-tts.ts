80c5afdc8860101b827ee47b1d7390bc7c2cb53a
Anashkin Denis <fnk007@yandex.ru>
2020-05-05 19:26:06 +0300

    set-tts command for changing voices

---
+++ src/commands/adm/set-tts.ts
@@ -0,0 +1,35 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { logger } from "../../logger";
+import yaTTS from "../../core/ya-tts";
+
+export default class SetTTS extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: "set-tts",
+      group: "adm",
+      memberName: "set-tts",
+      description:
+        'Настройки ТТС. Доступные значения `"oksana" | "jane" | "omazh" | "zahar" | "erkanyavas" | "alena" | "filipp" | "alyss" | "nick"`',
+      args: [
+        {
+          key: "voice",
+          prompt:
+            'Установить другой голос. Доступные значения `"oksana" | "jane" | "omazh" | "zahar" | "erkanyavas" | "alena" | "filipp" | "alyss" | "nick"`',
+          type: "string",
+        },
+      ],
+      clientPermissions: ["MANAGE_MESSAGES"],
+      userPermissions: ["MANAGE_MESSAGES"],
+    });
+  }
+
+  async run(message: CommandoMessage, { voice }: { voice: string }) {
+    try {
+      yaTTS.voice = voice;
+      return message.reply("успешно изменено");
+    } catch (error) {
+      logger.error(error);
+      return message.reply(error.message);
+    }
+  }
+}
