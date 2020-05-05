475f07e1e7a54054830d6acf6960d86bb4b38e16
Anashkin Denis <fnk007@yandex.ru>
2020-05-05 23:42:15 +0300

    Revert "postgres db"
    This reverts commit e6dfd545

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,9 @@
 module.exports = {
-  type: "postgres",
+  type: "sqlite",
   name: "default",
+  database: "./database.sqlite",
   synchronize: true,
+  logging: false,
   entities: ["out/entity/**/*.js"],
   migrations: ["out/migration/**/*.js"],
   subscribers: ["out/subscriber/**/*.js"],
