b7f334f6a5af662b5142f2866fe50f26773976cf
Anashkin Denis <fnk007@yandex.ru>
2020-05-09 12:44:44 +0300

    load .env before anything else

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
+// load .env
+import { config } from "dotenv";
+config();
+// load other stuff
 import "reflect-metadata";
 import { Engine } from "./core/engine";
 import { logger } from "./logger";
-
+// start
 new Engine().init().then(() => {
   logger.info("READY");
 });
