13a6b0e3bbf245f73289be3e5ca9053f7408e00d
Anashkin Denis <fnk007@yandex.ru>
2020-05-25 20:07:53 +0300

    fix stat ordering, auto prettier

--- src/commands/pidor/stat.ts
+++ src/commands/pidor/stat.ts
@@ -16,7 +16,8 @@ export default class Stat extends Command {
       args: [
         {
           key: "type",
-          prompt: "Опционально можно добавить параметр: me - статистика для себя, top - за текущий год, all - за все время",
+          prompt:
+            "Опционально можно добавить параметр: me - статистика для себя, top - за текущий год, all - за все время",
           type: "string",
           default: "top",
         },
@@ -24,19 +25,28 @@ export default class Stat extends Command {
     });
   }
 
-  async run(message: CommandoMessage, { type }: {type: string }) {
+  async run(message: CommandoMessage, { type }: { type: string }) {
     try {
       const results = await Database.getGuildResults(message.guild.id);
       if (results === undefined) {
-        return message.say('Похоже, пидоров здесь еще нет');
+        return message.say("Похоже, пидоров здесь еще нет");
       }
       switch (type) {
         case "all":
           const all = this.all(results);
           const allEmbed = new MessageEmbed()
-                              .setTitle('Результаты за все время')
-                              .setColor(0x42aaf5)
-                              .setDescription(all.map((item) => `${message.guild.member(new User(this.client, { id: item.userId }))?.displayName} - ${item.wins} ${(item.wins === 2 || item.wins === 3) ? 'раза' : 'раз'}`).join('\r\n'));
+            .setTitle("Результаты за все время")
+            .setColor(0x42aaf5)
+            .setDescription(
+              all
+                .map(
+                  (item) =>
+                    `${message.guild.member(new User(this.client, { id: item.userId }))?.displayName} - ${item.wins} ${
+                      item.wins === 2 || item.wins === 3 ? "раза" : "раз"
+                    }`,
+                )
+                .join("\r\n"),
+            );
           return message.embed(allEmbed);
         case "me":
           return message.reply(`ты был пидором дня ${this.personal(results, message.author.id)} раз`);
@@ -44,9 +54,18 @@ export default class Stat extends Command {
         default:
           const top = this.top(results);
           const topEmbed = new MessageEmbed()
-                              .setTitle('Результаты за текущий год')
-                              .setColor(0x42aaf5)
-                              .setDescription(top.map((item) => `${message.guild.member(new User(this.client, { id: item.userId }))?.displayName} - ${item.wins} ${(item.wins === 2 || item.wins === 3) ? 'раза' : 'раз'}`).join('\r\n'));
+            .setTitle("Результаты за текущий год")
+            .setColor(0x42aaf5)
+            .setDescription(
+              top
+                .map(
+                  (item) =>
+                    `${message.guild.member(new User(this.client, { id: item.userId }))?.displayName} - ${item.wins} ${
+                      item.wins === 2 || item.wins === 3 ? "раза" : "раз"
+                    }`,
+                )
+                .join("\r\n"),
+            );
           return message.embed(topEmbed);
       }
     } catch (error) {
@@ -57,18 +76,20 @@ export default class Stat extends Command {
 
   private personal(results: Pidor[], userId: string): number {
     const currentYear = new Date().getUTCFullYear();
-    return results.filter((pidor) => (pidor.winnerId === userId) && (currentYear === new Date(pidor.resultTimestamp*1000).getUTCFullYear())).length;
+    return results.filter(
+      (pidor) => pidor.winnerId === userId && currentYear === new Date(pidor.resultTimestamp * 1000).getUTCFullYear(),
+    ).length;
   }
 
-  private top(results: Pidor[]): { userId: string, wins: number }[] {
+  private top(results: Pidor[]): { userId: string; wins: number }[] {
     const currentYear = new Date().getUTCFullYear();
-    return this.all(results.filter((pidor) => currentYear === new Date(pidor.resultTimestamp*1000).getUTCFullYear()));
+    return this.all(results.filter((pidor) => currentYear === new Date(pidor.resultTimestamp * 1000).getUTCFullYear()));
   }
 
-  private all(results: Pidor[]): { userId: string, wins: number }[] {
-    let result: { userId: string, wins: number }[] = [];
-    const ordered = _.take(_.orderBy(_.groupBy(results, 'winnerId'), 'length'), 10);
-    for (let pidor of ordered) {
+  private all(results: Pidor[]): { userId: string; wins: number }[] {
+    const result: { userId: string; wins: number }[] = [];
+    const ordered = _.take(_.orderBy(_.groupBy(results, "winnerId"), "length", "desc"), 10);
+    for (const pidor of ordered) {
       result.push({ userId: pidor[0].winnerId, wins: pidor.length });
     }
     return result;
