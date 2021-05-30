c654fd284af7afb6ac1c97b43ef810abcddc2311
Denis A <fnk007@yandex.ru>
2021-05-30 23:18:38 +0300

    random color generator

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,6 @@ import { isEmpty, isNil } from "lodash";
 export function isNullishOrEmpty(val: any): val is null | undefined {
   return isNil(val) || isEmpty(val);
 }
+export function randomColor() {
+  return Math.floor(Math.random() * 16777215).toString(16);
+}
